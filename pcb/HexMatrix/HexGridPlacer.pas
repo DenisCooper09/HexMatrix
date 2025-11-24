53d9d0ca57e4e5e2b5ac6a455c4b32ebe3a8c0f3
Denis Cooper <denis.cooper09@gmail.com>
2025-11-24 19:54:02 +0000

    Loop through selected components
    FYI: Iterator yields components in descending order - no need for manual sorting.

--- pcb/HexMatrix/HexGridPlacer.pas
+++ pcb/HexMatrix/HexGridPlacer.pas
@@ -1,8 +1,31 @@
-Procedure ShowAMessage;
 Var
-   DefaultMessage;
+   PCBBoard : IPCB_Board;
+   Comp     : IPCB_Component;
+   CompIter : IPCB_BoardIterator;
+
 Begin
-     DefaultMessage := 'Hello, World';
-     ShowMessage(DefaultMessage);
+     PCBBoard := PCBServer.GetCurrentPCBBoard;
+     If PCBBoard = Nil Then
+     Begin
+          ShowMessage('No PCB open!');
+          Exit;
+     End;
+
+     CompIter := PCBBoard.BoardIterator_Create;
+     CompIter.AddFilter_ObjectSet(MkSet(eComponentObject));
+     CompIter.AddFilter_Method(MkSet(eAllComponents));
+
+     Comp := CompIter.FirstPCBObject;
+     While Comp <> Nil Do
+     Begin
+          If Comp.Selected Then
+          Begin
+               ShowMessage('Component: ' + Comp.Name.Text);
+          End;
+
+          Comp := CompIter.NextPCBObject;
+     End;
+
+     PCBBoard.BoardIterator_Destroy(CompIter);
 End;
 
