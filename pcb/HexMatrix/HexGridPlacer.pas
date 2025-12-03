72de756458bed872c90cff0897301faab6f2fd0d
Denis Cooper <denis.cooper09@gmail.com>
2025-12-03 21:20:11 +0000

    Add axial coordinates generation

--- pcb/HexMatrix/HexGridPlacer.pas
+++ pcb/HexMatrix/HexGridPlacer.pas
@@ -1,11 +1,40 @@
+Procedure PlaceComponentsInHexGrid(Size : Integer, CompList : TInterfaceList);
+Var
+   Comp             : IPCB_Component;
+   i                : Integer;
+
+   r, q, dq, q0, q1 : Integer;
+   qLeft, qRight    : Integer;
+
+   LeftRight        : Boolean;
+
+Begin
+     For r := Size - 1 DownTo -Size + 1 Do
+     Begin
+          qLeft     := -r * Ord(r < 0)  - (Size - 1);
+          qRight    := -r * Ord(r >= 0) + (Size - 1);
+
+          LeftRight := (r And 1) Xor (Size And 1);
+
+          dq        := IfThen(LeftRight, 1, -1);
+          q0        := IfThen(LeftRight, qLeft, qRight);
+          q1        := IfThen(LeftRight, qRight, qLeft);
+
+          q := q0;
+          While q <> q1 + dq Do
+          Begin
+               ShowMessage(IntToStr(q) + ' ' + IntToStr(r));
+               q := q + dq;
+          End;
+     End;
+End;
+
 Var
    PCBBoard : IPCB_Board;
 
    Comp     : IPCB_Component;
    CompIter : IPCB_BoardIterator;
    CompList : TInterfaceList;
-
-   i        : Integer;
 
 Begin
      PCBBoard := PCBServer.GetCurrentPCBBoard;
@@ -32,10 +61,6 @@ Begin
 
      PCBBoard.BoardIterator_Destroy(CompIter);
 
-     For i := CompList.Count - 1 DownTo 0 Do
-     Begin
-          Comp := CompList[i];
-          ShowMessage('Component: ' + Comp.Name.Text);
-     End;
+     PlaceComponentsInHexGrid(3, CompList);
 End;
 
